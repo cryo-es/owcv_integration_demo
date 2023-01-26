d65eb41209ec9fbf4dff71a8b2bf65824a3925f2
cryo-es <118654007+cryo-es@users.noreply.github.com>
2023-01-26 13:37:23 +0000

    Update ow_cv_class.py

--- demo/ow_cv_class.py
+++ demo/ow_cv_class.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 as cv
-from mss.windows import MSS as mss
+import dxcam
 import os
 
 
@@ -26,10 +26,7 @@ class ComputerVision:
 			"damage_beam":[658, 719, 1065, 1126],
 			"resurrect_cd":[920, 1000, 1580, 1655],
 			"being_beamed":[762, 807, 460, 508]}
-		#extremes = self.get_extremes()
-		#self.screenshot_region = {"top":extremes[0], "left":extremes[2], "height":extremes[1]-extremes[0], "width":extremes[3]-extremes[2]}
-		self.screenshot_region = {"top":0, "left":0, "height":final_resolution["height"], "width":final_resolution["width"]}
-		#self.scale_to_res(self.final_resolution, extremes)
+		self.screenshot_region = (0, 0, final_resolution["width"], final_resolution["height"])
 		self.templates = {}
 		for i in self.coords:
 			self.templates[i] = cv.cvtColor(cv.imread(resource_path(f"t_{i}.png")), cv.COLOR_RGB2GRAY)
@@ -37,59 +34,35 @@ class ComputerVision:
 		self.masks = {}
 		for i in self.mask_names:
 			self.masks[i] = cv.cvtColor(cv.imread(resource_path(f"m_{i}.png")), cv.COLOR_RGB2GRAY)
-		self.screen = mss()
+		self.screen = dxcam.create(output_color="GRAY")
 		self.frame = []
 
-	#def get_extremes(self):
-	#	extremes = [99999, 0, 99999, 0]
-	#	for i in self.coords.values():
-	#		if i[0] < extremes[0]:
-	#			extremes[0] = i[0]
-	#		if i[1] > extremes[1]:
-	#			extremes[1] = i[1]
-	#		if i[2] < extremes[2]:
-	#			extremes[2] = i[2]
-	#		if i[3] > extremes[3]:
-	#			extremes[3] = i[3]
-	#	return extremes
+	def start_capturing(self, capture_fps=60):
+		self.screen.start(target_fps=capture_fps, video_mode=True)
 
-	#def scale_to_res(self, final_resolution, extremes):
-	#	multiplier = {
-	#	"height" : final_resolution["height"] / self.resolution["height"],
-	#	"width" : final_resolution["width"] / self.resolution["width"]}
-	#	for i in self.coords:
-	#		self.coords[i][0] = int(self.coords[i][0]-extremes[0] * multiplier["height"])
-	#		self.coords[i][1] = int(self.coords[i][1]-extremes[0] * multiplier["height"])
-	#		self.coords[i][2] = int(self.coords[i][2]-extremes[2] * multiplier["width"])
-	#		self.coords[i][3] = int(self.coords[i][3]-extremes[2] * multiplier["width"])
-	#	self.resolution = final_resolution
+	def stop_capturing(self):
+		self.screen.stop()
 
 	def capture_frame(self):
 		if self.final_resolution != self.base_resolution:
-			self.frame = cv.cvtColor(cv.resize(np.array(self.screen.grab(self.screenshot_region)), (self.base_resolution["width"], self.base_resolution["height"])), cv.COLOR_RGB2GRAY)
+			self.frame = cv.resize(self.screen.get_latest_frame(), (self.base_resolution["width"], self.base_resolution["height"]))
 		else:
-			self.frame = cv.cvtColor(np.array(self.screen.grab(self.screenshot_region)), cv.COLOR_RGB2GRAY)
+			self.frame = self.screen.get_latest_frame()
 
 	def crop(self, image, template_name):
 		return image[self.coords[template_name][0]:self.coords[template_name][1], self.coords[template_name][2]:self.coords[template_name][3]]
 
-	def detect_multiple(self, template_name):
+	def match(self, template_name):
 		if template_name in self.mask_names:
-			result = cv.matchTemplate(self.crop(self.frame, template_name), self.templates[template_name], cv.TM_CCOEFF_NORMED, mask=self.masks[template_name])
+			return cv.matchTemplate(self.crop(self.frame, template_name), self.templates[template_name], cv.TM_CCOEFF_NORMED, mask=self.masks[template_name])
 		else:
-			result = cv.matchTemplate(self.crop(self.frame, template_name), self.templates[template_name], cv.TM_CCOEFF_NORMED)
+			return cv.matchTemplate(self.crop(self.frame, template_name), self.templates[template_name], cv.TM_CCOEFF_NORMED)
+
+	def detect_multiple(self, template_name):
+		result = self.match(template_name)
 		cv.threshold(result, .9, 255, cv.THRESH_BINARY, result)
 		return len(cv.findContours(result.astype(np.uint8), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[0])
 
 	def detect_single(self, template_name):
-		if template_name in self.mask_names:
-			result = cv.matchTemplate(self.crop(self.frame, template_name), self.templates[template_name], cv.TM_CCOEFF_NORMED, mask=self.masks[template_name])
-		else:
-			result = cv.matchTemplate(self.crop(self.frame, template_name), self.templates[template_name], cv.TM_CCOEFF_NORMED)
+		result = self.match(template_name)
 		return result.max() > .9
-
-#a = ComputerVision()
-#a.frame = cv.cvtColor(cv.imread(resource_path("resurrected.png"))[66:1000, 41:1655], cv.COLOR_RGB2GRAY)
-#cv.imshow("image", a.crop(a.frame, "resurrect_cd"))
-#cv.waitKey(0)
-#a.detect_single("resurrect_cd")
